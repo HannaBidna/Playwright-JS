f5f508a098f5746ca8500be9eaa1a37af2156c62
/anya_skazala <130675463+HannaBidna@users.noreply.github.com>
2024-07-24 12:56:07 +0200

    Update base.ts
    changes

--- scr/fixtures/base.ts
+++ scr/fixtures/base.ts
@@ -1,15 +1,13 @@
-export * from '@playwright/test';
-
 import { test as base } from '@playwright/test';
-import { LoginPage } from '../pages/pages/login-page';
-import { DriverPage } from '../pages/pages/driver-page';
-import { TruckPage } from '../pages/pages/trucks-page';
-import { Steps } from '../steps';
-import { API } from '../helpers/api';
+import { InOutPage } from '../pages/loginPage';
+import { DriverPage } from '../pages/driverPage';
+import { TruckPage } from '../pages/trucksPage';
+import { Steps } from '../pages/helpers/step';
+import { API } from '../pages/helpers/api';
 
 
 type MyFixtures = {
-    loginPage: LoginPage;
+    loginPage: InOutPage;
     driverPage: DriverPage;
     truckPage: TruckPage;
     steps: Steps;
@@ -17,7 +15,7 @@ type MyFixtures = {
 };
 
 export const test = base.extend<MyFixtures>({
-    loginPage: async ({ page }, use) => await use(new LoginPage(page)),
+    loginPage: async ({ page }, use) => await use(new InOutPage(page)),
     driverPage: async ({ page }, use) => await use(new DriverPage(page)),
     truckPage: async ({ page }, use) => await use(new TruckPage(page)),
     steps: async({}, use) => await use(new Steps()),
