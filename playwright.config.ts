8250238b2a40091c78c9a77379c1b2cb85165a33
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2024-08-01 15:40:29 +0200

    new

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,35 +1,21 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { users } from '../playwright/scr/pages/helpers/users';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://dev.omni-dispatch.com/',
+  projects: [
+    {
+      name: 'testUser',
+      use: {
+        user: users.testUser,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+        baseURL: 'https://dev.omni-dispatch.com/',
+      },
+    },
+  ],
+});
+
+
 
   /* Configure projects for major browsers */
   projects: [
@@ -75,4 +61,6 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+
+
+
